438b7f3b3bad920a9ee0bb838ade76fb1a09c991
VictorONN <VictNO@protonmail.com>
2023-03-07 21:41:24 +0300

    a few updates

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,45 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="App">
-      <Switch>
-        <Route path="/abouts">
-          <About />
-        </Route>
-        <Route path="/users">
-          <Users />
-        </Route>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        {" "}
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item active">
+              <Link className="nav-link" to="/">
+                Home <span className="sr-only">(current)</span>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/abouts">
+                Abouts
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/users">
+                Users
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+      <div className="App">
+        <Routes>
+          <Route path="/abouts">
+            <About />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Routes>
+      </div>
     </div>
   );
 }
